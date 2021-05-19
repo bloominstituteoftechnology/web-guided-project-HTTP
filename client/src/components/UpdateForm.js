52429d5368d0126ee950e7b68317377bcd7a0c21
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-05-19 13:40:02 -0400

    lecture work-in-progress

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -12,7 +12,8 @@ const initialItem = {
 
 const UpdateForm = props => {
   const [item, setItem] = useState(initialItem);
-  const id = props.match.params.id;
+  // const id = props.match.params.id;
+  const history = useHistory();
 
   useEffect(()=>{
     axios.get(`http://localhost:3333/items/${id}`)
